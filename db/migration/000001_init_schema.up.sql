db204337017d3223daa804baa081059e446ab8ce
SaeedSH02 <iamsaeed6469@gmail.com>
2026-09-09 17:19:46 +0330

    fix(db): correct typos in initial schema

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,42 +1,42 @@
-CREATE TABLE "acccunts" (
+CREATE TABLE "accounts" (
   "id" bigserial PRIMARY KEY,
   "owner" varchar NOT NULL,
   "balance" bigint NOT NULL,
   "currency" varchar NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "entries" (
   "id" bigserial PRIMARY KEY,
-  "acccunt_id" bigint NOT NULL,
+  "account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
 CREATE TABLE "transfers" (
   "id" bigserial PRIMARY KEY,
-  "from_acccunt_id" bigint NOT NULL,
-  "to_acccunt_id" bigint NOT NULL,
+  "from_account_id" bigint NOT NULL,
+  "to_account_id" bigint NOT NULL,
   "amount" bigint NOT NULL,
-  "created_at" timestamptz NOT NULL DEFAULT 'now()'
+  "created_at" timestamptz NOT NULL DEFAULT (now())
 );
 
-CREATE INDEX ON "acccunts" ("owner");
+ALTER TABLE "entries" ADD FOREIGN KEY ("account_id") REFERENCES "accounts" ("id");
 
-CREATE INDEX ON "entries" ("acccunt_id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("from_account_id") REFERENCES "accounts" ("id");
 
-CREATE INDEX ON "transfers" ("from_acccunt_id");
+ALTER TABLE "transfers" ADD FOREIGN KEY ("to_account_id") REFERENCES "accounts" ("id");
 
-CREATE INDEX ON "transfers" ("to_acccunt_id");
+CREATE INDEX ON "accounts" ("owner");
 
-CREATE INDEX ON "transfers" ("from_acccunt_id", "to_acccunt_id");
+CREATE INDEX ON "entries" ("account_id");
 
-COMMENT ON COLUMN "entries"."amount" IS 'can be negative or pasitive';
+CREATE INDEX ON "transfers" ("from_account_id");
+
+CREATE INDEX ON "transfers" ("to_account_id");
+
+CREATE INDEX ON "transfers" ("from_account_id", "to_account_id");
+
+COMMENT ON COLUMN "entries"."amount" IS 'can be negative or positive';
 
 COMMENT ON COLUMN "transfers"."amount" IS 'must be positive';
-
-ALTER TABLE "entries" ADD FOREIGN KEY ("acccunt_id") REFERENCES "acccunts" ("id") DEFERRABLE INITIALLY IMMEDIATE;
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("from_acccunt_id") REFERENCES "acccunts" ("id") DEFERRABLE INITIALLY IMMEDIATE;
-
-ALTER TABLE "transfers" ADD FOREIGN KEY ("to_acccunt_id") REFERENCES "acccunts" ("id") DEFERRABLE INITIALLY IMMEDIATE;
