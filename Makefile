fd94e1132b66c3815d0054d4b32dd3e67db295aa
SaeedSH02 <iamsaeed6469@gmail.com>
2026-09-20 03:04:36 +0330

    feat: add http api

--- Makefile
+++ Makefile
@@ -13,4 +13,7 @@ sqlc:
 test:
 	go test -v -cover ./...
 
-.PHONY: createdb dropdb postgres12 migrateup migratedown sqlc test
+server:
+	go run main.go
+
+.PHONY: createdb dropdb postgres12 migrateup migratedown sqlc test server
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/SaeedSH02/simplebank/api"
+	db "github.com/SaeedSH02/simplebank/db/sqlc"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbSource     = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	severAddress = "0.0.0.0:8080"
+)
+
+func main() {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(severAddress)
+	if err != nil {
+		log.Fatal("cannot start server:", err)
+	}
+}
